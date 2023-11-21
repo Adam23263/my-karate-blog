dca2cdfb2286fc3dd064e4cf732dd7259f20328c
Adam <92024341+mich-am@users.noreply.github.com>
2023-11-21 16:15:46 +0000

    Update config.js add kobudo

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -64,13 +64,13 @@ export default defineConfig({
           },
             text: 'Kobudo',
             collapsed: false,
-            link: '/main/coming_soon'
+            link: '/main/coming_soon',
 /*
             items:[
               {text: 'Bojutsu', link: '/main/kobudo/bojutsu'}
             ]
-          },
 */
+          },
           {
             text: 'Side Quests',
             collapsed: true,
